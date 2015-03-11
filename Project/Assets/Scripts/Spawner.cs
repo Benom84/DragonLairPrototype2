c600d36b9d240937ea7ee0406034036963586efd
ThaliaDeShalit <thaliapenguin@gmail.com>
2015-03-11 21:47:51 +0200

    Spawner

--- Project/Assets/Scripts/Spawner.cs
+++ Project/Assets/Scripts/Spawner.cs
@@ -3,13 +3,25 @@ using System.Collections;
 
 public class Spawner : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	public float spawnTime = 5f;		// The amount of time between each spawn.
+	public float spawnDelay = 3f;		// The amount of time before spawning starts.
+	public GameObject[] enemies;		// Array of enemy prefabs.
 	
+	
+	void Start () {
+		// Start calling the Spawn function repeatedly after a delay .
+		InvokeRepeating("Spawn", spawnDelay, spawnTime);
 	}
 	
-	// Update is called once per frame
-	void Update () {
 	
+	void Spawn () {
+		// Instantiate a random enemy.
+		int enemyIndex = Random.Range(0, enemies.Length);
+		Instantiate(enemies[enemyIndex], transform.position, transform.rotation);
+		
+		// Play the spawning effect from all of the particle systems.
+		foreach(ParticleSystem p in GetComponentsInChildren<ParticleSystem>()) {
+			p.Play();
+		}
 	}
 }
