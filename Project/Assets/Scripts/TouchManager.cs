c4b09018138f8b406654b6870462da272c0d9172
Benom84 <menahem.ben@gmail.com>
2015-03-12 20:33:51 +0200

    TimerBar Graphics

--- Project/Assets/Scripts/TouchManager.cs
+++ Project/Assets/Scripts/TouchManager.cs
@@ -7,6 +7,8 @@ public class TouchManager : MonoBehaviour
     private Player player;
     private GameObject gameArea;
     private GameObject[] dragonAttackButtons;
+    private GameObject[] specialAttackButtons;
+    private GameObject selectedSpecialAttack;
     private int attackTouchIndex = -1;
 
     // Use this for initialization
@@ -16,6 +18,8 @@ public class TouchManager : MonoBehaviour
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
         gameArea = transform.FindChild("GameArea").gameObject;
         dragonAttackButtons = GameObject.FindGameObjectsWithTag("AttackChooser");
+        specialAttackButtons = GameObject.FindGameObjectsWithTag("SpecialAttack");
+        Debug.Log("Size of special attacks is: " + specialAttackButtons.Length);
 
     }
 
@@ -39,12 +43,29 @@ public class TouchManager : MonoBehaviour
                     attackChooserTouchHandler(touchPos, i, j);
 
 
+            // If the touch is on the special moves - TODO
+            for (int j = 0; j < specialAttackButtons.Length; j++)
+            {
+                //Debug.Log("Checking if it is a special");
+                if (specialAttackButtons[j].collider2D == Physics2D.OverlapPoint(touchPos))
+                {
+                    specialAttackTouchHandler(touchPos, i, j);
+                    Debug.Log("The index is: " + j);
+                }
+                    
+            }
+                
+                
+            
+
+            
+            
             // Making sure that no matter where the attack touch ended it is initialize
             if ((attackTouchIndex == i) && (Input.GetTouch(i).phase == TouchPhase.Ended))
                 attackTouchIndex = -1;
 
 
-            // If the touch is on the special moves - TODO
+            
 
             // If the touch is on another UI - In the future
 
@@ -97,9 +118,28 @@ public class TouchManager : MonoBehaviour
     {
 
         if (Input.GetTouch(touchIndex).phase == TouchPhase.Began)
-            player.setActiveAttack(dragonAttackButtons[touchIndex].GetComponent<DragonAttackButton>().attack);
+            player.setActiveAttack(dragonAttackButtons[attackButtonIndex].GetComponent<DragonAttackButton>().attack);
 
     }
-    private void specialMovesTouchHandler(Vector2 touchPos, int touchIndex) { }
+    private void specialAttackTouchHandler(Vector2 touchPos, int touchIndex, int specialAttackIndex) 
+    {
+        Debug.Log("The specialAttackTouchHandler was called");
+
+        if (Input.GetTouch(touchIndex).phase == TouchPhase.Began)
+        {
+            
+            selectedSpecialAttack = specialAttackButtons[specialAttackIndex];
+            Debug.Log("A touch has began on " + selectedSpecialAttack.name);
+        }
+
+
+
+        if ((selectedSpecialAttack == specialAttackButtons[specialAttackIndex]) && (Input.GetTouch(touchIndex).phase == TouchPhase.Moved))
+        {
+            selectedSpecialAttack.GetComponent<SpecialAttackButton>().setButtonPosition(touchPos);
+            Debug.Log("A touch is going on on " + selectedSpecialAttack.name);
+        }
+            
+    }
 
 }
