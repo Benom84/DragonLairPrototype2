52e470be85815fcf549ab1540422c859f0ea7913
ThaliaDeShalit <thaliapenguin@gmail.com>
2015-03-12 14:06:31 +0200

    HealthBar Enemies

--- Project/Assets/Scripts/Enemy.cs
+++ Project/Assets/Scripts/Enemy.cs
@@ -6,7 +6,7 @@ public class Enemy : MonoBehaviour
 
     public enum EnemyType { Knight, Cavalier, Healer, Archer };
 
-    public int health = 100;
+    public float maxHealth = 100f;
     public int attackDamage = 1;
     public float attackDelay = 1.0f;
     public float walkingSpeed = 3.0f;
@@ -15,10 +15,14 @@ public class Enemy : MonoBehaviour
     public float resistenceToAcid = 0.0f;
     public EnemyType enemyType;
 
-    private Player player;
     private GameController gameController;
     private bool arrivedAtDestination = false;
     private float lastAttackTime = 0.0f;
+    private float health;
+    private Player player;
+
+    private SpriteRenderer healthBar;			// Reference to the sprite renderer of the health bar.
+    private Vector3 healthScale;				// The local scale of the health bar initially (with full health).
 
 
     // Use this for initialization
@@ -28,6 +32,12 @@ public class Enemy : MonoBehaviour
         walkingSpeed = walkingSpeed / 100;
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
         gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+
+        health = maxHealth;
+        healthBar = transform.Find("ui_healthDisplay/Enemy_HealthBar").GetComponent<SpriteRenderer>();
+        healthScale = healthBar.transform.localScale;
+        healthBar.transform.localScale = new Vector3(healthScale.x * health/maxHealth, 1, 1);
+
 
     }
 
@@ -53,8 +63,6 @@ public class Enemy : MonoBehaviour
 
         }
 
-
-
     }
 
     void OnTriggerEnter2D(Collider2D col)
@@ -76,7 +84,8 @@ public class Enemy : MonoBehaviour
     {
 
         health -= damage;
-        if (health <= 0)
+        UpdateHealthBar();
+        if (health <= 0f)
             Death();
 
     }
@@ -92,5 +101,14 @@ public class Enemy : MonoBehaviour
         lastAttackTime = Time.time;
         player.Hit(attackDamage);
 
+    }
+
+    public void UpdateHealthBar()
+    {
+        // Set the health bar's colour to proportion of the way between green and red based on the player's health.
+        healthBar.material.color = Color.Lerp(Color.green, Color.red, 1 - health / maxHealth);
+
+        // Set the scale of the health bar to be proportional to the player's health.
+        healthBar.transform.localScale = new Vector3(healthScale.x * health / maxHealth, 1, 1);
     }
 }
