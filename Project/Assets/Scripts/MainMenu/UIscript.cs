90ed92da4a3557d3b3aa8e1563c9ae754e951a1b
ThaliaDeShalit <thaliapenguin@gmail.com>
2015-04-17 11:45:20 +0300

    Store

---
+++ Project/Assets/Scripts/MainMenu/UIscript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIscript : MonoBehaviour
+{
+
+    public void LoadScene(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+}
