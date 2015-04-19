fbba2bf827d96f4864f685f3f822fb926f615a34
Benom84 <menahem.ben@gmail.com>
2015-04-19 16:52:21 +0300

    nana

--- Project/Assets/Scripts/DataController.cs
+++ Project/Assets/Scripts/DataController.cs
@@ -9,47 +9,47 @@ public class DataController : MonoBehaviour {
     public static DataController dataController;
 
     //general state of player
-    public int level;
-    public int coins;
-    public int crystals;
+    public int level = 1;
+    public int coins = 0;
+    public int crystals = 3;
 
     //general upgrades
-    public int p_cave;
-    public int m_scream;
-    public int m_earthquake;
+    public int p_cave = 100;
+    public int m_scream = 12;
+    public int m_earthquake = 50;
 
     //fire upgrades
-    public int b_fireDamage;
-    public int b_fireRange;
-    public int b_fireHeavnlyFire;
-    public int b_fireThunder;
-    public int p_fireLava;
-    public int m_fireMeteor;
+    public int b_fireDamage = 50;
+    public int b_fireRange = 10;
+    public int b_fireHeavnlyFire = 50;
+    public int b_fireThunder = 50;
+    public int p_fireLava = 50;
+    public int m_fireMeteor = 50;
 
     //air upgrades
-    public int b_airDamage;
-    public int b_airRange;
-    public int b_airSkyFall;
-    public int b_airCursedBreath;
-    public int p_airTornado;
-    public int m_airTornado;
+    public int b_airDamage = 50;
+    public int b_airRange = 10;
+    public int b_airSkyFall = 50;
+    public int b_airCursedBreath = 50;
+    public int p_airTornado = 50;
+    public int m_airTornado = 50;
 
     //water upgrades
-    public int b_waterDamage;
-    public int b_waterRange;
-    public int b_waterFrozenSky;
-    public int b_waterCursedBreath;
-    public int p_waterIceWall;
-    public int m_waterMist;
+    public int b_waterDamage = 50;
+    public int b_waterRange = 10;
+    public int b_waterFrozenSky = 50;
+    public int b_waterCursedBreath = 50;
+    public int p_waterIceWall = 50;
+    public int m_waterMist = 50;
 
     public DragonAttack.AttackType attackType;
 
     //info to pass from level to victory/lose scene
-    public int kills;
-    public int life;
-    public int coinsFromStage;
-    public int crystalsFromStage;
-    public bool won;
+    public int kills = 0;
+    public int life = 0;
+    public int coinsFromStage = 0;
+    public int crystalsFromStage = 0;
+    public bool won = false;
 
 	void Awake () {
         if (dataController == null)
