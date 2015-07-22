758aa99ea24395e3e6f783fc5b7e8c578a9669f1
Benom84 <menahem.ben@gmail.com>
2015-07-22 21:17:21 +0300

    Sound

--- Project/Assets/Sound.cs
+++ Project/Assets/Sound.cs
@@ -4,16 +4,53 @@ using System.Collections;
 public class Sound : MonoBehaviour {
 
     public static Sound sound;
+    public static float volume = 0.4f;
+
+    private AudioSource audio;
 
 	public void Start () {
         if (sound == null)
         {
             DontDestroyOnLoad(gameObject);
             sound = this;
+            audio = GetComponent<AudioSource>();
+            if (!DataController.dataController.isMusicOn)
+            {
+                if (audio != null)
+                {
+                    audio.volume = 0;
+                }
+            }
+            else
+            {
+                if (audio != null)
+                {
+                    audio.volume = volume;
+                }
+            }
+            audio.Play();
         }
         else if (sound != this)
         {
             Destroy(gameObject);
         }
 	}
+
+    void Update()
+    {
+        if (!DataController.dataController.isMusicOn)
+        {
+            if (audio != null)
+            {
+                audio.volume = 0;
+            }
+        }
+        else
+        {
+            if (audio != null)
+            {
+                audio.volume = volume;
+            }
+        }
+    }
 }
